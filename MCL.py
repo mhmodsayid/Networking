cdf3443b42dbf01b9f31f2ce87903f57861c36a6
mhmod <mh_mouds@hotmail.com>
2019-09-29 13:55:43 +0300

    almost there

--- MCL.py
+++ MCL.py
@@ -1,22 +1,13 @@
-import sys
-import random
-import string
-import decimal
+
 import numpy as np
-from decimal import Decimal
-from array import array
-decimal.getcontext().prec=100
-import os
-import sys
 
 def main():
     power=2
     inflate=2
     done=0
     i=2894#nodes number
-    Clusters=94
     clastering= np.zeros(shape=(i),dtype=int)
-    np.set_printoptions(precision=2)
+    np.set_printoptions(precision=10)
     a = np.zeros(shape=(i,i))
     data = np.loadtxt("HW2Net.net",skiprows=2,dtype=int)
     #for x in np.nditer(data,flags=['multi_index']):
@@ -37,40 +28,41 @@ def main():
         temp=c
         for j in range(power-1):
             c=c.dot(c)
+    
         #multiply
         c=np.power(c,inflate)
         for k in range(i):
             c[:,k]=c[:,k]/np.sum(c[:,k])
-        c=np.round(c,4)
+        c=np.round(c,10)
         done+=1
         print(done)
         
-    #print(a, end=' ')
-   
-    for j in range(2893):
-        print(j,np.count_nonzero(c[j+1,:]!=0))
-
     Cnumber=0
     for j in range(2893):
         if c[j,j]!=0:
             count=np.count_nonzero(c[j,:]!=0)
             if count>1:
                 np.place(clastering, c[j,:]!=0, Cnumber)
-                Cnumber+=1
+                if Cnumber<92:
+                    Cnumber+=1
+            
             else:
-                np.place(clastering, c[j,:]!=0,-1)
+                np.place(clastering, c[j,:]!=0,93)
+            
         else:
-            clastering[j]=-2
-        
+            clastering[j]=94
         
     
             
      #   *Vertices 2894.
     
     #num.savetxt('clustering.clu', DAT, delimiter=" ", fmt="%s") 
-    
-    np.savetxt('First_matrix.txt', c, delimiter=',',fmt='%1.4f')
-    np.savetxt('clustering.clu', clastering, delimiter=',',fmt='%1.0f')
+    with open("clustering P="+str(power)+",I="+str(inflate)+".clu", "wb") as f:
+        f.write(b"*Vertices 2894\n")
+        np.savetxt(f, clastering, delimiter=',',fmt='%1.0f')
+
+    np.savetxt('First_matrix.txt', c, delimiter=',',fmt='%1.10f')
+ #   np.savetxt('clustering.clu', clastering, delimiter=',',fmt='%1.0f')
 
 
    
